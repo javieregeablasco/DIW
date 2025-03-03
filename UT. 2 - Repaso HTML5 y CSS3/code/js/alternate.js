2c3918fd6fdbab8fb1f9f30a19453177de846190
javieregeablasco <j.egeablasco@edu.gva.es>
2025-03-03 19:35:06 +0000

    Mar 3, 2025, 8:35 PM

--- UT. 2 - Repaso HTML5 y CSS3/code/js/alternate.js
+++ UT. 2 - Repaso HTML5 y CSS3/code/js/alternate.js
@@ -1,5 +1,5 @@
 function toggleMode() {
-  let links = document.querySelectorAll('link[rel="stylesheet"], link[rel="alternate stylesheet"]');
+  let links = document.querySelectorAll('link[rel="canonical"], link[rel="alternate"]');
   links.forEach(link => {
       link.disabled = !link.disabled;
   });
